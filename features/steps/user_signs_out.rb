9226f18fd0bfef0a06be68b99cd3741ba91db019
Tibor Claassen <tibor@gmah.net>
2012-07-13 13:18:00 +0200

    Feature: user signs out

---
+++ features/steps/user_signs_out.rb
@@ -0,0 +1,29 @@
+class UserSignsOut < Spinach::FeatureSteps
+  Given 'my name is "William Blake" with login "Nobody" and password "se7en!"' do
+    pending 'step not implemented'
+  end
+
+  And 'I am logged in' do
+    pending 'step not implemented'
+  end
+
+  When 'I click to open the footer' do
+    pending 'step not implemented'
+  end
+
+  And 'cklick on "Log out"' do
+    pending 'step not implemented'
+  end
+
+  Then 'I should not be within the application' do
+    pending 'step not implemented'
+  end
+
+  But 'should see the login form' do
+    pending 'step not implemented'
+  end
+
+  And 'should see a notice "Successfully logged out"' do
+    pending 'step not implemented'
+  end
+end
--- features/user_signs_out.feature
+++ features/user_signs_out.feature
@@ -5,4 +5,10 @@ Feature: User signs out
   I want to log off the repository
 
   Scenario: log out
-    When   
+    Given my name is "William Blake" with login "Nobody" and password "se7en!"
+    And I am logged in
+    When I click to open the footer
+    And cklick on "Log out"
+    Then I should not be within the application
+    But should see the login form
+    And should see a notice "Successfully logged out"
