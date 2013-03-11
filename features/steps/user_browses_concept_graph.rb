d15c45f5d58379bc912ed05f58b285ecb3530a43
Tibor Claassen <tibor@gmah.net>
2013-03-11 17:30:48 +0100

    Feature spec

--- features/steps/user_browses_concept_graph.rb
+++ features/steps/user_browses_concept_graph.rb
@@ -4,16 +4,7 @@ class UserBrowsesConceptGraph < Spinach::FeatureSteps
   include AuthSteps
   include SearchSteps
   include Api::Graph::Factory
-
-  def collect_edges
-    page.evaluate_script <<-JS
-      $("#coreon-concept-map .concept-edge").map( function() {
-        source = this.__data__.source.model.get("label");
-        target = this.__data__.target.model.get("label");
-        return source + " -> " + target;
-      }).get();
-    JS
-  end
+  include EdgesHelpers
 
   step 'a concept "handgun"' do
     @concept = create_concept_with_label "handgun"
---
+++ features/steps/user_toggles_orientation_of_concept_map.rb
@@ -0,0 +1,49 @@
+class Spinach::Features::UserTogglesOrientationOfConceptMap < Spinach::FeatureSteps
+  include AuthSteps
+  include Api::Graph::Factory
+  include EdgesHelpers
+
+  step 'a concept "handgun"' do
+    @concept = create_concept_with_label "handgun"
+  end
+
+  step 'this concept is narrower than "weapon"' do
+    @weapon ||= create_concept_with_label "weapon"
+    link_narrower_to_broader @concept, @weapon
+  end
+
+  step 'I visit the single concept page for "handgun"' do
+    visit "/concepts/#{@concept['_id']}"
+  end
+
+  step 'I should see "handgun" being selected in the concept map' do
+    page.should have_css("#coreon-concept-map .concept-node.hit", text: "handgun")
+  end
+
+  step 'it should be connected to "weapon"' do
+    @edges = collect_edges
+    @edges.should include("weapon -> handgun")
+  end
+
+  def get_position(label)
+    page.evaluate_script "$('.concept-node:contains(#{label})').position()"
+  end
+
+  step '"weapon" should be rendered left of "handgun"' do
+    weapon_position = get_position "weapon"
+    handgun_position = get_position "handgun"
+    weapon_position["left"].should < handgun_position["left"]
+  end
+
+  step 'I click "Toggle orientation"' do
+    page.click_on "Toggle orientation"
+  end
+
+  step '"handgun" should still be selected' do
+    pending 'step not implemented'
+  end
+
+  step '"weapon" should be rendered above "handgun"' do
+    pending 'step not implemented'
+  end
+end
