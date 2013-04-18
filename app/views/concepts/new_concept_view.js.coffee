292ca1f620348b6d5ed448dcffa8dbfc7532c529
Norman Köhring <n@koehr.in>
2013-04-18 13:56:18 +0200

    inbetween the add-term-scenario

--- app/views/concepts/new_concept_view.js.coffee
+++ app/views/concepts/new_concept_view.js.coffee
@@ -5,6 +5,7 @@
 #= require templates/concepts/_caption
 #= require templates/concepts/new_concept
 #= require templates/properties/new_property
+#= require templates/terms/new_term
 #= require views/concepts/shared/broader_and_narrower_view
 #= require models/concept
 #= require jquery.serializeJSON
@@ -15,16 +16,20 @@ class Coreon.Views.Concepts.NewConceptView extends Backbone.View
 
   template: Coreon.Templates["concepts/new_concept"]
   property: Coreon.Templates["properties/new_property"]
+  term: Coreon.Templates["terms/new_term"]
 
   events:
     "click  a.add-property"    : "addProperty"
     "click  a.remove-property" : "removeProperty"
+    "click  a.add-term"        : "addTerm"
+    "click  a.remove-term"     : "removeTerm"
     "submit form"              : "create"
 
   initialize: ->
     @broaderAndNarrower = new Coreon.Views.Concepts.Shared.BroaderAndNarrowerView
       model: @model
     @propCount = 0
+    @termCount = 0
 
   render: ->
     @$el.html @template concept: @model
@@ -39,6 +44,13 @@ class Coreon.Views.Concepts.NewConceptView extends Backbone.View
 
   removeProperty: (event) ->
     $(event.target).closest(".property").remove()
+
+  addTerm: (event) ->
+    @termCount += 1
+    @$(".terms").append @term index: @termCount - 1
+
+  removeTerm: (event) ->
+    $(event.target).closest(".term").remove()
 
   create: (event) ->
     event.preventDefault()
--- features/steps/maintainer_creates_concept.rb
+++ features/steps/maintainer_creates_concept.rb
@@ -66,16 +66,35 @@ class MaintainerCreatesConcept < Spinach::FeatureSteps
     end
   end
 
+  step 'I should see a set of term inputs with labels "Value", "Language"' do
+    within(".term") do
+      page.should have_field("Value")
+      page.should have_field("Language")
+    end
+  end
+
   step 'I click "Add property"' do
     page.click_link "Add property"
+  end
+
+  step 'I click "Add term"' do
+    page.click_link "Add term"
   end
 
   step 'I click "Remove property" within the set' do
     page.click_link "Remove property"
   end
 
+  step 'I click "Remove term" within the set' do
+    page.click_link "Remove term"
+  end
+
   step 'I should not see a set of property inputs anymore' do
     page.should have_no_css(".property")
+  end
+
+  step 'I should not see a set of term inputs anymore' do
+    page.should have_no_css(".term")
   end
 
   step 'I fill "Key" with "label"' do
@@ -86,13 +105,30 @@ class MaintainerCreatesConcept < Spinach::FeatureSteps
     page.fill_in "Value", with: "dead man"
   end
 
+  step 'I fill "Value" with "corpse"' do
+    page.fill_in "Value", with: "corpse"
+  end
+
+  step 'I fill "Language" with "en"' do
+    page.fill_in "Language", with: "en"
+  end
+
   step 'I should see "dead man" within the title' do
     page.should have_css(".concept h2", text: "dead man")
+  end
+
+  step 'I should see "corpse" within the title' do
+    page.should have_css(".concept h2", text: "corpse")
   end
 
   step 'I should see a property "LABEL" with value "dead man"' do
     page.should have_css(".properties th", text: "LABEL")
     page.find(:xpath, "//th[text() = 'label']/following-sibling::td").text.should == "dead man"
+  end
+
+  step 'I should see an English term "corpse"' do
+    binding.pry
+    page.should have_css(".terms .language.en .term h4", text: "corpse")
   end
 
   step 'I do a search for "corpse"' do
