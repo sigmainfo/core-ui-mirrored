e5ee740215777e0439614b82541aca1a3465fc7a
Tibor Claassen <tibor@gmah.net>
2013-04-26 22:37:42 +0200

    Remove fake service and kill acceptance environment

--- config/deploy/acceptance.rb
+++
@@ -1,8 +0,0 @@
-set :deploy_to, "/var/www/rails"
-set :rails_env, "acceptance"
-set :branch, "acceptance" 
-set :repository_cache, "cached-acceptance"
-
-role :web, "10.42.1.50"
-role :app, "10.42.1.50"
-role :db,  "10.42.1.50", :primary => true
--- server/controllers/application_controller.rb
+++ server/controllers/application_controller.rb
@@ -1,13 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  private
-
-  Api::Graph::NotAuthenticated = Class.new(StandardError)
-
-  def authenticate
-    unless Api::Auth::User.where(id: request.headers["X-Core-Session"][0...24]).exists?
-      render json: {
-      }, status: 403
-    end
-  end
 end
