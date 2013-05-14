ac35fcd9a9eaf9aa379b55a883af956a82661a18
Tibor Claassen <tibor@gmah.net>
2013-05-14 22:07:27 +0200

    Define feature "maintainer deletes concept"

---
+++ features/maintainer_deletes_concept.feature
@@ -0,0 +1,27 @@
+@wip
+Feature: maintainer deletes concept
+  In order to make a concept no longer available
+  As a maintainer editing data in the repository
+  I want to remove an existing concept
+
+  Background:
+    Given my name is "William Blake" with login "Nobody" and password "se7en!"
+    And I am logged in
+    And a concept "beaver hat" exists
+  
+  Scenario: remove concept
+    Given I have maintainer privileges
+    And I am on the show concept page of this concept
+    # When I click "Edit concept" 
+    When I click "Delete concept"
+    Then I should see a confirmation dialog "This concept including all terms will be deleted permanently."
+    When I click outside the dialog
+    Then I should not see a confirmation dialog
+    And I should still be on the show concept page
+    When I click "Delete concept"
+    And I click "OK" within the dialog
+    Then I should be on the repository root page
+    And I should not see a confirmation dialog
+    And I should see a message 'Successfully deleted concept "beaver hat".'
+    When I search for "hat"
+    Then I should not see "beaver hat"
---
+++ features/steps/maintainer_deletes_concept.rb
@@ -0,0 +1,61 @@
+class Spinach::Features::MaintainerDeletesConcept < Spinach::FeatureSteps
+  step 'I have maintainer privileges' do
+    pending 'step not implemented'
+  end
+
+  step 'I am on the show concept page of this concept' do
+    pending 'step not implemented'
+  end
+
+  step 'I click "Delete concept"' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see a confirmation dialog "This concept including all terms will be deleted permanently."' do
+    pending 'step not implemented'
+  end
+
+  step 'I click outside the dialog' do
+    pending 'step not implemented'
+  end
+
+  step 'I should not see a confirmation dialog' do
+    pending 'step not implemented'
+  end
+
+  step 'I should still be on the show concept page' do
+    pending 'step not implemented'
+  end
+
+  step 'I click "OK" within the dialog' do
+    pending 'step not implemented'
+  end
+
+  step 'I should be on the repository root page' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see a message \'Successfully deleted concept "beaver hat".\'' do
+    pending 'step not implemented'
+  end
+
+  step 'I search for "hat"' do
+    pending 'step not implemented'
+  end
+
+  step 'I should not see "beaver hat"' do
+    pending 'step not implemented'
+  end
+
+  step 'my name is "William Blake" with login "Nobody" and password "se7en!"' do
+    pending 'step not implemented'
+  end
+
+  step 'I am logged in' do
+    pending 'step not implemented'
+  end
+
+  step 'a concept "beaver hat" exists' do
+    pending 'step not implemented'
+  end
+end
