024a5d658ffe1acc5a1db0488b66100929a3c01f
Tibor Claassen <tibor@gmah.net>
2013-08-02 11:24:37 +0200

    Use Poltergeist to fix konacha:run in Ruby 2.0

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,4 +1,11 @@
+Capybara.server do |app, port|
+  require 'rack/handler/thin'
+  Rack::Handler::Thin.run(app, :Port => port)
+end
+
+require 'capybara/poltergeist'
+
 Konacha.configure do |config|
   config.spec_dir  = "spec"
-  config.driver    = :webkit
+  config.driver    = :poltergeist
 end if defined?(Konacha)
