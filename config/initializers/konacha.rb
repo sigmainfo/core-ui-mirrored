448e522f717bf21e6e10033c8bad787fee6f5b49
Tibor Claassen <tibor@gmah.net>
2013-08-12 17:37:49 +0200

    Poltergeist will wait longer for app to boot

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,7 +1,13 @@
-Capybara.server do |app, port|
-  require 'rack/handler/thin'
-  Rack::Handler::Thin.run(app, :Port => port)
-end if defined?(Capybara)
+if defined?(Capybara)
+  Capybara.server do |app, port|
+    require 'rack/handler/thin'
+    Rack::Handler::Thin.run(app, :Port => port)
+  end
+
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, timeout: 60)
+  end
+end
 
 Konacha.configure do |config|
   require 'capybara/poltergeist'
