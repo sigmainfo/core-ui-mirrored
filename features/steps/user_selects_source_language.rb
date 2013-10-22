7e45b61b03a626203348bd0d6a45eedde4cde34d
Tibor Claassen <tibor@gmah.net>
2013-10-22 17:46:40 +0200

    [Feature] User selects source language

---
+++ features/steps/user_selects_source_language.rb
@@ -0,0 +1,49 @@
+class Spinach::Features::UserSelectsSourceLanguage < Spinach::FeatureSteps
+  step 'the languages "English", "German", and "French" are available' do
+    pending 'step not implemented'
+  end
+
+  step 'I visit the repository root page' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see a widget "Languages"' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see selection "None" for "Source language"' do
+    pending 'step not implemented'
+  end
+
+  step 'I click the trigger next to "Source Language"' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see a dropdown with "None", "English", "German", and "French"' do
+    pending 'step not implemented'
+  end
+
+  step 'I select "German" from the dropdown' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see selection "German" for "Source language"' do
+    pending 'step not implemented'
+  end
+
+  step 'I should not see a dropdown' do
+    pending 'step not implemented'
+  end
+
+  step 'I reload the page' do
+    pending 'step not implemented'
+  end
+
+  step 'my name is "William Blake" with email "nobody@blake.com" and password "se7en!"' do
+    pending 'step not implemented'
+  end
+
+  step 'I am logged in' do
+    pending 'step not implemented'
+  end
+end
---
+++ features/user_selects_source_language.feature
@@ -0,0 +1,22 @@
+@wip
+Feature: user selects source language
+  In order to display and search terms from a source language
+  As a user browsing the repository
+  I want to set a search language
+
+  Background:
+    Given my name is "William Blake" with email "nobody@blake.com" and password "se7en!"
+    And I am logged in
+
+  Scenario: select source language
+    Given the languages "English", "German", and "French" are available
+    When I visit the repository root page
+    Then I should see a widget "Languages"
+    And I should see selection "None" for "Source language"
+    When I click the trigger next to "Source Language"
+    Then I should see a dropdown with "None", "English", "German", and "French"
+    When I select "German" from the dropdown
+    Then I should see selection "German" for "Source language"
+    And I should not see a dropdown
+    When I reload the page
+    Then I should see selection "German" for "Source language"
