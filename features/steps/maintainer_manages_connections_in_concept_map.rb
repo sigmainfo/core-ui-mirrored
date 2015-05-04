45068d57837cff436e35bb9ca53c6d2d781e5942
sigmainfo <sigmainfo>
2015-05-04 15:47:02 +0530

    added maintainer_manages_connections_in_concept_map steps

---
+++ features/steps/maintainer_manages_connections_in_concept_map.rb
@@ -0,0 +1,125 @@
+class Spinach::Features::MaintainerManagesConnectionsInConceptMap < Spinach::FeatureSteps
+  step 'I can move the map with drag and drop on empty space' do
+    pending 'step not implemented'
+  end
+
+  step 'I can zoom in the map' do
+    pending 'step not implemented'
+  end
+
+  step 'I click on the "intra-EU transport" concept node' do
+    pending 'step not implemented'
+  end
+
+  step 'I am still in the root node page' do
+    pending 'step not implemented'
+  end
+
+  step 'in this view there is a "Reset" button' do
+    pending 'step not implemented'
+  end
+
+  step 'in this view there is a "Cancel" button' do
+    pending 'step not implemented'
+  end
+
+  step 'in this view there is a "Save relations" button' do
+    pending 'step not implemented'
+  end
+
+  step 'I drag concept "pipeline transport" on the "destination of transport" concept' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "pipeline transport" connected with a thick line with concept "destination of transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "pipeline transport" connected with a dotted line with concept "mode of transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I click "Save relations"' do
+    pending 'step not implemented'
+  end
+
+  step 'I am not in edit mode' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "pipeline transport" connected with concept "destination of transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "intra-EU transport" connected with concept "destination of transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "mode of transport" does not heave any subconcepts' do
+    pending 'step not implemented'
+  end
+
+  step 'I click "Reset"' do
+    pending 'step not implemented'
+  end
+
+  step 'I am in edit mode' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "destination of transport" connected with concept "intra-EU transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "mode of transport" connected with concept "pipeline transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I see concept "pipeline transport" has no connection with concept "destination of transport"' do
+    pending 'step not implemented'
+  end
+
+  step 'I click "Cancel"' do
+    pending 'step not implemented'
+  end
+
+  step 'I am logged in as maintainer of the repository' do
+    pending 'step not implemented'
+  end
+
+  step 'a concept with label "destination of transport" exists' do
+    pending 'step not implemented'
+  end
+
+  step 'the concept "destination of transport" has "intra-EU transport" as a subconcept' do
+    pending 'step not implemented'
+  end
+
+  step 'a concept with label "mode of transport" exists' do
+    pending 'step not implemented'
+  end
+
+  step 'the concept "mode of transport" has "pipeline transport" as a subconcept' do
+    pending 'step not implemented'
+  end
+
+  step 'I visit the repository root page' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see a widget "Concept Map"' do
+    pending 'step not implemented'
+  end
+
+  step 'I click on "Maximize" inside the widget "Concept Map"' do
+    pending 'step not implemented'
+  end
+
+  step 'I see a concept map inside the main view' do
+    pending 'step not implemented'
+  end
+
+  step 'I click the "Edit mode" button' do
+    pending 'step not implemented'
+  end
+end
